2c49b1cca4b1f43d5810d0015e0bdd090f3c26aa
mohamed junaid <mohamedjunaid888@gmail.com>
2025-10-02 15:34:46 +0530

    Time: 15 ms (82.94%), Space: 70.8 MB (88.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let map=new Set()
-    for(let i=0;i<nums.length;i++){
-        if(map.has(nums[i])){
-            return true
-        }else{
-            map.add(nums[i])
-        }
-    }
-    return false
+    let map=new Set(nums)
+    if (map.size<nums.length){
+        return true
+    }else{return false}
+
 };
