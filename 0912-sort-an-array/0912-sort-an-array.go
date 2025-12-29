d59ff515eadb8c9fdd89b13a3bb394fca43770bb
Mohamed Junaid <mohamedjunaid888@gmail.com>
2025-12-29 10:11:58 +0530

    Time: 60 ms (45%), Space: 19.2 MB (12%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.go
+++ 0912-sort-an-array/0912-sort-an-array.go
@@ -1,5 +1,27 @@
 func sortArray(nums []int) []int {
+    if len(nums)<=1{
+        return nums
+    }
+    mid:=len(nums)/2
+    left:=sortArray(nums[:mid])
+    right:=sortArray(nums[mid:])
+    return merge(left,right)
+}
 
- slices.Sort(nums)
-return nums 
+func merge(left,right []int)[]int{
+    r:=make([]int,0,len(left)+len(right))
+    i,j:=0,0
+    
+    for i<len(left)&&j<len(right){
+        if left[i]<=right[j]{
+            r=append(r,left[i])
+            i++
+        }else{
+            r=append(r,right[j])
+            j++
+        }
+    }
+    r=append(r,left[i:]...)
+    r=append(r,right[j:]...)
+    return r
 }
