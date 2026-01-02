95c6804ec3d803ff3f5927f53570075b21d83cf9
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-01-02 21:11:07 +0530

    Time: 6 ms (71.43%), Space: 9.2 MB (69.84%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.go
@@ -0,0 +1,30 @@
+type ParkingSystem struct {
+    m map[int]int
+}
+
+
+func Constructor(big int, medium int, small int) ParkingSystem {
+      return ParkingSystem{
+        m: map[int]int{
+            1: big,
+            2: medium,
+            3: small,
+        },
+    }
+}
+
+
+func (this *ParkingSystem) AddCar(carType int) bool {
+     if this.m[carType] > 0 {
+        this.m[carType]--
+        return true
+    }
+    return false
+}
+
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * obj := Constructor(big, medium, small);
+ * param_1 := obj.AddCar(carType);
+ */
