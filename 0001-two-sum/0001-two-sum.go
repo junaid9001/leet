6d324eea183040cc45178b13cb070d03c14fa1b7
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-01-25 11:05:58 +0530

    Time: 0 ms (100%), Space: 5.8 MB (63.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,13 +1,12 @@
 func twoSum(nums []int, target int) []int {
 	m := map[int]int{}
-	var ans []int
+
 
 	for key, val := range nums {
 		comp := target - val
 		_, ok := m[comp]
 		if ok {
-			ans = append(ans, []int{key, m[comp]}...)
-			return ans
+			return []int{key, m[comp]}
 		} else {
 			m[val] = key
 		}
