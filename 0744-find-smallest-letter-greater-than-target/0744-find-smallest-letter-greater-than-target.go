dbdc38bbe615b6782b8fe05d2f96f264aa98979c
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-02-03 12:00:10 +0530

    Time: 0 ms (100%), Space: 4.7 MB (26.69%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.go
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.go
@@ -1,11 +1,9 @@
 func nextGreatestLetter(letters []byte, target byte) byte {
-    for key,v:=range letters{
+    for _,v:=range letters{
         if v>target{
             return v
         }
-        if key==len(letters)-1{
-            return letters[0]
-        }
+    
     }
-    return byte(0)
+    return letters[0]
 }
