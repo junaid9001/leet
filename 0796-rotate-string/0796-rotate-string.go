9f07e225e91fc82d55c3ee374d4617e089909908
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-02-10 12:31:17 +0530

    Time: 0 ms (100%), Space: 3.9 MB (89.24%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.go
+++ 0796-rotate-string/0796-rotate-string.go
@@ -1,18 +1,3 @@
 func rotateString(s string, goal string) bool {
-    if len(s)!=len(goal){
-        return false
-    }
-    if s==goal{
-        return true
-    }
-	b := []byte(s)
-
-	for i := 0; i < len(s); i++ {
-		first:=b[0]
-		b = append(b[1:],byte(first))
-		if string(b) == goal {
-			return true
-		}
-	}
-	return false
+    return len(s)==len(goal)&&strings.Contains(s+s,goal)
 }
