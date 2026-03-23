96702f6f0dd65eb56426c6126c9f1ca316435059
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-03-23 15:40:38 +0530

    Time: 0 ms (100%), Space: 4.4 MB (60.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.go
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.go
@@ -1,18 +1,18 @@
 func uniqueOccurrences(arr []int) bool {
     m:=make(map[int]int)
-    count:=make(map[int]bool)
     for _,val:=range arr{
         m[val]++
     }
-    fmt.Println(m)
+
+
+   seen:=make(map[int]bool)
 
     for _,v:=range m{
-        _,ok:=count[v]
-        if !ok{
-            count[v]=true
+        if !seen[v]{
+            seen[v]=true
             continue
         }
-        return false
+        return false 
     }
     return true
 }
