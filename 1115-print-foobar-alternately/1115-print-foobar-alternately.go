c967f5255a7d6c813ff0db7586832c376fa22413
Mohamed Junaid <mohamedjunaid888@gmail.com>
2026-05-26 22:12:31 +0530

    Time: 7 ms (8.77%), Space: 8.7 MB (22.06%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.go
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.go
@@ -1,11 +1,11 @@
 type FooBar struct {
 	n      int
-	first  chan bool
-	second chan bool
+	first  chan struct{}
+	second chan struct{}
 }
 
 func NewFooBar(n int) *FooBar {
-	return &FooBar{n: n, first: make(chan bool), second: make(chan bool)}
+	return &FooBar{n: n, first: make(chan struct{}), second: make(chan struct{})}
 }
 
 func (fb *FooBar) Foo(printFoo func()) {
@@ -15,7 +15,7 @@ func (fb *FooBar) Foo(printFoo func()) {
 		}
 		// printFoo() outputs "foo". Do not change or remove this line.
 		printFoo()
-		fb.first <- true
+		fb.first <- struct{}{}
 	}
 }
 
@@ -25,7 +25,7 @@ func (fb *FooBar) Bar(printBar func()) {
 		// printBar() outputs "bar". Do not change or remove this line.
 		printBar()
 		if i < fb.n-1 {
-			fb.second <- true
+			fb.second <- struct{}{}
 		}
 
 	}
